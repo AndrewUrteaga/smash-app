43f2a25e25c0a82fdccb62585db0a4f5b7c12ee3
AndrewUrteaga <aurte002@ucr.edu>
2019-04-01 16:02:16 -0700

    first commit and push

---
+++ smashappV2/Front/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlayersComponent,
+    NavBarComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
